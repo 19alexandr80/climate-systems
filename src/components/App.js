3b01e72569a300472a43fd5e7017c6c1e986a221
19alexandr80 <usanka1980@gmail.com>
2024-05-13 13:08:17 +0300

    router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import Header from "./Header/Header";
-import Main from "./Main/Main";
-import Footer from "./Footer/Footer";
+// import Header from "./Header/Header";
+// import Main from "./Main/Main";
+// import Footer from "./Footer/Footer";
+
+import { NavLink, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <div>
+      <nav>
+        <NavLink to="/">home</NavLink>
+        <NavLink to="/doogs">Peges</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h2>home</h2>} />
+        <Route path="/doogs" element={<h2>Peges</h2>} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="climate-systems">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
 reportWebVitals();
-//  https://github.com/19alexandr80/climate-systems.git
-// .github/workflows/deploy.yml
+// https://github.com/19alexandr80/climate-systems.git
