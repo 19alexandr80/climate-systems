0a400403ff492dad6f165f1ca84bf5588e7d19a7
19alexandr80 <usanka1980@gmail.com>
2024-05-17 06:08:36 +0300

    vvv

--- src/components/Main/Slider/Slider.jsx
+++ src/components/Main/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import fotoA from "images/slaider/H3kFnC4y.jpg";
+import fotoA from "images/slaider/vent.webp";
 import fotoB from "images/slaider/HE9lk19c.jpg";
 import fotoC from "images/slaider/derevo_tuman_mgla_147791_1920x1080.jpg";
 import fotoD from "images/slaider/j6ZFSqEW.jpg";
@@ -9,23 +9,25 @@ import * as s from "./Slider.styled";
 export default function Slider() {
   return (
     <>
-      <s.SliderList>
-        <li>
-          <img src={fotoA} alt="iconSlider" width={100} height={100} />
-        </li>
-        <li>
-          <img src={fotoB} alt="iconSlider" width={100} height={100} />
-        </li>
-        <li>
-          <img src={fotoC} alt="icon" width={100} height={100} />
-        </li>
-        <li>
-          <img src={fotoD} alt="icon" width={100} height={100} />
-        </li>
-        <li>
-          <img src={fotoE} alt="icon" width={100} height={100} />
-        </li>
-      </s.SliderList>
+      <s.Carusel>
+        <s.SliderList>
+          <s.Slide>
+            <s.SlideImg src={fotoA} alt="iconSlider" width={100} height={100} />
+          </s.Slide>
+          <s.Slide>
+            <s.SlideImg src={fotoB} alt="iconSlider" width={100} height={100} />
+          </s.Slide>
+          <s.Slide>
+            <s.SlideImg src={fotoC} alt="icon" width={100} height={100} />
+          </s.Slide>
+          <s.Slide>
+            <s.SlideImg src={fotoD} alt="icon" width={100} height={100} />
+          </s.Slide>
+          <s.Slide>
+            <s.SlideImg src={fotoE} alt="icon" width={100} height={100} />
+          </s.Slide>
+        </s.SliderList>
+      </s.Carusel>
     </>
   );
 }
--- src/components/Main/Slider/Slider.styled.js
+++ src/components/Main/Slider/Slider.styled.js
@@ -1,7 +1,53 @@
 import styled from "styled-components";
 
-export const SliderList = styled.ul`
+export const Carusel = styled.div`
+  overflow-x: clip;
+  width: 100%;
+  /* overflow-x: clip;
+  max-width: 350px;
+  padding-left: 18px;
+  @media (max-width: 355px) {
+    max-width: 315px;
+  }
+  @media (min-width: 1024px) {
+    padding-left: 18px;
+  }
+  @media (min-width: 1280px) {
+    padding-left: 18px;
+  } */
+`;
+
+export const SliderList = styled.div`
   display: flex;
-  list-style: none;
+  /* list-style: none; */
   transition: 1s all;
+  padding: 0px;
+  margin: 0px;
+`;
+export const Slide = styled.div`
+  /* width: 100vw; */
+  min-width: 100%;
+  max-width: 100%;
+  height: 450px;
+  /* padding-left: 60px;
+  padding-right: 60px; */
+  /* margin-right: 25px;
+  @media (min-width: 1024px) {
+    margin-right: 40px;
+  } */
+`;
+export const SlideImg = styled.img`
+  width: 100%;
+  height: 100%;
+
+  /* max-width: 245px;
+  border-radius: 2px;
+  box-shadow: 2px 2px 15px 4px rgb(0 0 0 / 25%);
+  @media (min-width: 1024px) {
+    width: 194px;
+  }
+  @media (min-width: 1280px) {
+    max-width: 254px;
+    width: 254px;
+  } */
 `;
