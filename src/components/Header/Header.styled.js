21495ff37297ee5fa7bd4f6c2c4ea0ea52a40a38
19alexandr80 <usanka1980@gmail.com>
2024-09-16 11:13:45 +0300

    404on

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const HeaderBlock = styled.nav`
-  background-color: red;
+  background-color: gray;
 `;
 export const HeadBl = styled.div`
   display: flex;
