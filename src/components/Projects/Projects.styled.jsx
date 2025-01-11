ad71247c6b1330cfafc3f27accb17ebe8fb67212
19alexandr80 <usanka1980@gmail.com>
2025-01-11 14:34:35 +0200

    loader

--- src/components/Projects/Projects.styled.jsx
+++ src/components/Projects/Projects.styled.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 export const ProjectsDiv = styled.div`
   background: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5));
-  background-position: center;
   background-size: cover;
   width: 100%;
   min-height: 630px;
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Modal from "./Modal/Modal";
+import { Loader } from "./Loader/Loader";
 
 export const Layout = () => {
   const [modal, setModal] = useState("false");
@@ -19,7 +20,7 @@ export const Layout = () => {
     <>
       <Header onOpen={onOpen} />
       <main>
-        <Suspense fallback={<div>...Loading</div>}>
+        <Suspense fallback={<Loader />}>
           <Outlet />
         </Suspense>
       </main>
